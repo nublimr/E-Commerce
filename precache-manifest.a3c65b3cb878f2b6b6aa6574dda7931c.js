b4199486390f6149ef98e12e34695a4da4752a25
nublimr <nubliezrapat@gmail.com>
2019-07-13 02:10:04 +0700

    first commit

---
+++ precache-manifest.a3c65b3cb878f2b6b6aa6574dda7931c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbb185448c556773420360d0fecfb4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a815f926d7cad0ee8682",
+    "url": "/static/css/main.c7b781d1.chunk.css"
+  },
+  {
+    "revision": "f6d913c3874862c7d8fa",
+    "url": "/static/js/2.a789515c.chunk.js"
+  },
+  {
+    "revision": "a815f926d7cad0ee8682",
+    "url": "/static/js/main.7574ef01.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8b0e38d89cd73b44eb89abc6c059833b",
+    "url": "/static/media/Product1_1.8b0e38d8.png"
+  },
+  {
+    "revision": "7030ffe11bacf5c537ef6dd9c6428f52",
+    "url": "/static/media/Product1_2.7030ffe1.png"
+  },
+  {
+    "revision": "99080d10c427f8f51a0f08cf8263391e",
+    "url": "/static/media/Product1_3.99080d10.png"
+  },
+  {
+    "revision": "b5a224b66cfad1f43931f92209b33998",
+    "url": "/static/media/Product2_1.b5a224b6.png"
+  },
+  {
+    "revision": "4200cc9abb6b43f2a38908846df8495f",
+    "url": "/static/media/Product2_2.4200cc9a.png"
+  },
+  {
+    "revision": "79e66ed231f6de1e576a2364fb48922b",
+    "url": "/static/media/Product2_3.79e66ed2.png"
+  },
+  {
+    "revision": "7f9a8db702c7ba3f5ee40f47229165c3",
+    "url": "/static/media/Product3_1.7f9a8db7.png"
+  },
+  {
+    "revision": "f6c89f9ff083a372519c5c1746861ae3",
+    "url": "/static/media/Product3_2.f6c89f9f.png"
+  },
+  {
+    "revision": "1edd07719e004306821f52e2433dd183",
+    "url": "/static/media/Product3_3.1edd0771.png"
+  },
+  {
+    "revision": "f88ed50ead96bd4d87fd7f0e9c488cfe",
+    "url": "/static/media/Product4_1.f88ed50e.png"
+  },
+  {
+    "revision": "b820951517744ab0dea68aef36736dec",
+    "url": "/static/media/Product4_2.b8209515.png"
+  },
+  {
+    "revision": "3a3cb797b534d6d57eb7f94c64adf752",
+    "url": "/static/media/Product4_3.3a3cb797.png"
+  },
+  {
+    "revision": "487bdc1adceb50c5f18ad2e66f832dd8",
+    "url": "/static/media/Product5_1.487bdc1a.png"
+  },
+  {
+    "revision": "0c7f67e5a52b87b6c16c8bc9a28e3fff",
+    "url": "/static/media/Product5_2.0c7f67e5.png"
+  },
+  {
+    "revision": "bbee5fe1ab36b9e5853e801191f05665",
+    "url": "/static/media/Product5_3.bbee5fe1.png"
+  },
+  {
+    "revision": "bd43752fdcd08f63e581abf666ca2e60",
+    "url": "/static/media/Product6_1.bd43752f.png"
+  },
+  {
+    "revision": "450ca3965bfd36b07ba52cb817ce3dbd",
+    "url": "/static/media/Product6_2.450ca396.png"
+  },
+  {
+    "revision": "45bce4364cee950faf550982f1f4f948",
+    "url": "/static/media/Product6_3.45bce436.png"
+  },
+  {
+    "revision": "347864f374499f9c988ad2c6f556d949",
+    "url": "/static/media/Product7_1.347864f3.png"
+  },
+  {
+    "revision": "9f20f9cba66b3892b59d2c1b4c134c37",
+    "url": "/static/media/Product7_2.9f20f9cb.png"
+  },
+  {
+    "revision": "df6fc9f4f975e39ce21ebfe633033ede",
+    "url": "/static/media/Product7_3.df6fc9f4.png"
+  },
+  {
+    "revision": "17f8e4a49874feaeca0c9ff38a84f365",
+    "url": "/static/media/Product8_1.17f8e4a4.png"
+  },
+  {
+    "revision": "389bf0bdaa98fdf74aa3a59a8e0a9881",
+    "url": "/static/media/Product8_2.389bf0bd.png"
+  },
+  {
+    "revision": "a3c62ea1ec0099979d0156590c981fbb",
+    "url": "/static/media/Product8_3.a3c62ea1.png"
+  }
+]);
